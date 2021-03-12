e8b15be5e99a56a659a8f53b43e302680b5c0fad
Tyler Cheney <tyler.cheney.05@gmail.com>
2021-03-11 23:35:56 -0700

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ages = (10..90).to_a
+5.times do
+  doctor = Doctor.create(
+      doctor_name: Faker::GreekPhilosophers.name,
+      specialty: Faker::Games::Minecraft.enchantment 
+  )
+  3.times do
+    patient = Patient.create(
+    patient_name: Faker::FunnyName.name,
+    age: ages.sample,
+    gender: Faker::Gender.binary_type 
+  )
+    2.times do
+      Appointment.create(
+        date: Faker::Date.between(from: '1523-09-23', to: '1876-09-25'),
+        reason: Faker::Games::Pokemon.move,
+        notes: Faker::ChuckNorris.fact,
+        patient_id: patient.id,
+        doctor_id: doctor.id
+      )
+    end
+  end
+end
+puts "Data Seeded"
